2096f575f8979c343ea143e594534c0e3a6a7467
Mizanur-Choudhury <70804958+Mizanur-Choudhury@users.noreply.github.com>
2023-04-28 05:47:20 +0100

    # variables are what we use in python to set the information that we are going to us in our program and retrieving/using it.

---
+++ section_1_intro_to_python/variables_and_printing.py
@@ -0,0 +1,27 @@
+# variables are what we use in python to set the information that we are going to us in our program and
+# retrieving/using it.
+# The syntax for variables is typed text followed by a = and the user input Example: VARIABLE = miz
+age = 30
+
+# in order to print the user text we can use print() and in the brackets, input our variable or a input directly
+# Example: print(VARIABLE)
+# Example: print(30)
+
+print(age)
+print(30)
+
+friend_age = 23
+
+# in python, we use underscores to separate words in longer variables. This is called SNAKE CASE
+countries_visited = 90
+
+# We can also use numbers in our variables, however WE CAN'T USE VARIABLES that start with numbers!
+countries_visited_v2 = 93
+
+# in python, the best practices is if you have variables that will never change,
+# the code should be register in upper case
+PI = 3.14159
+
+RADIANS_TO_DEGREES = 180 / PI
+
+print(RADIANS_TO_DEGREES)
