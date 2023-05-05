a73708ac095fd8e88a2be17998b082491b633eab
Mizanur-Choudhury <70804958+Mizanur-Choudhury@users.noreply.github.com>
2023-05-05 07:28:32 +0100

    how to us modulus for odd or even numbers

---
+++ section_1_intro_to_python/modulus for odd or even numbers.py
@@ -0,0 +1,12 @@
+"""
+using the modulus operator,
+We can calculate if a number is odd or even based on its REMAINDER.
+
+0 = EVEN
+1 = ODD
+"""
+
+x = 897  # Any number goes here
+remainder = x % 2
+
+print(remainder)
